e931013aa921f86fe98fdf6a3ee3b6f20f0dfc06
TOGoS <togos00@gmail.com>
2017-07-18 12:14:06 +0200

    Make map previews dir a variable

--- bin/initvars.sh
+++ bin/initvars.sh
@@ -3,5 +3,6 @@
 factorio_dir="${factorio_dir:-../Factorio}"
 factorio_exe="${factorio_exe:-${factorio_dir}/bin/debug/factorio}"
 
+map_previews_dir="map-previews"
 map_width="${map_width:-1024}"
 mapgen_seed="${mapgen_seed:-1}"
