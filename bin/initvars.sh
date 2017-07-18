718785ee8cd4fca96db926f208569ba7589a4807
TOGoS <togos00@gmail.com>
2017-07-18 12:11:19 +0200

    Improve generate-test-map-previews

---
+++ bin/initvars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+factorio_dir="${factorio_dir:-../Factorio}"
+factorio_exe="${factorio_exe:-${factorio_dir}/bin/debug/factorio}"
+
+map_width="${map_width:-1024}"
+mapgen_seed="${mapgen_seed:-1}"
