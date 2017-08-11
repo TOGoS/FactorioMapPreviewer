b47167e8a059b70cb0d3bf33ea853d9cdd89d383
TOGoS <togos00@gmail.com>
2017-08-11 01:08:53 -0500

    Allow specifying multiple seeds to generate-test-map-previews

--- bin/initvars.sh
+++ bin/initvars.sh
@@ -5,4 +5,4 @@ factorio_exe="${factorio_exe:-${factorio_dir}/bin/debug/factorio}"
 
 map_previews_dir="map-previews"
 map_width="${map_width:-1024}"
-mapgen_seed="${mapgen_seed:-1}"
+mapgen_seeds="${mapgen_seed:-1}"
