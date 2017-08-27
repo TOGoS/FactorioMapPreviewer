e95d37c93d0b9c2111e9202d54bffa7a68dca3e9
TOGoS <togos00@gmail.com>
2017-08-26 20:18:06 -0500

    Support indicating a mapgen settings file

--- bin/initvars.sh
+++ bin/initvars.sh
@@ -5,5 +5,6 @@ factorio_exe="${factorio_exe:-${factorio_dir}/bin/debug/factorio}"
 
 map_previews_dir="map-previews"
 map_width="${map_width:-1024}"
+mapgen_settings_file="${mapgen_settings_file:-}"
 mapgen_seeds="${mapgen_seed:-1}"
 slope_shading="${slope_shading:-0.3}"
